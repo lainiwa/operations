69133bbaae832389bf88ada67e6d14d34c66dd44
lainiwa <kirrik96@gmail.com>
2022-11-03 00:35:39 +0300

    Update settings in config.py file
    Add password for DB connection
    Add reading settings from environment variables
    Pick better defaults

--- rates/src/rates/config.py
+++ rates/src/rates/config.py
@@ -1,5 +1,8 @@
+import os
+
 DB = {
-    "name": "postgres",
-    "user": "postgres",
-    "host": "localhost"
+    "name": os.getenv("RATES_DB_NAME", default="rates"),
+    "user": os.getenv("RATES_DB_USER", default="admin"),
+    "host": os.getenv("RATES_DB_HOST", default="localhost"),
+    "password": os.getenv("RATES_DB_PASSWORD", default="password"),
 }
